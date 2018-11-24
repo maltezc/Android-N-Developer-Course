a385c10fc59af0f1845733ab50407fe59d60324e
cflux1990 <cflux.maltez@live.com>
2018-11-24 10:33:27 -0800

    3.40-App Number Shapes

---
+++ 3.39 - App Number Shapes/src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+
+    public static void main(String[] args) {
+        class Number {
+            int number;
+
+            public boolean isSquare() {
+                double squareRoot = Math.sqrt(number);
+
+                if (squareRoot == Math.floor(squareRoot)) {
+                    return true;
+
+                } else {
+                    return false;
+                }
+
+            }
+
+
+            public boolean isTriangular () {
+                int x = 1;
+                int triangularNumber = 1;
+
+                while (triangularNumber < number) {
+                    x++;
+                    triangularNumber = triangularNumber + x;
+
+                }
+                if (triangularNumber == number) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        Number myNumber = new Number();
+        myNumber.number = 9;
+
+        System.out.println(myNumber.isSquare());
+    }
+
+}

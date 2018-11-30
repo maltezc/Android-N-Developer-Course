f1e60de51d99545f21451ab23cfea6b11a457050
cflux1990 <cflux.maltez@live.com>
2018-11-29 20:29:48 -0800

    4.54 - Audio Part1

--- 4.53-SoundDemo/app/src/main/java/com/example/chrismaltez/sounddemo/MainActivity.java
+++ 4.53-SoundDemo/app/src/main/java/com/example/chrismaltez/sounddemo/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.chrismaltez.sounddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mPlayer;
+
+
+    public void audioStart(View view) {
+
+
+        mPlayer.start();
+    }
+
+    public void audioPause(View view) {
+
+        mPlayer.stop();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPlayer = MediaPlayer.create(this, R.raw.bellstibetandanielsimon);
     }
 }

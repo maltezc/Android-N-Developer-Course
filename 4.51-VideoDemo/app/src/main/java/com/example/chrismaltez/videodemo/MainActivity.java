9aa06a14efbf5dda785fbaf42a256b2d09b57d4c
cflux1990 <cflux.maltez@live.com>
2018-11-29 19:44:10 -0800

    4.51-Video

---
+++ 4.51-VideoDemo/app/src/main/java/com/example/chrismaltez/videodemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.chrismaltez.videodemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+
+        videoView.setVideoPath("android.resource://" + getPackageName() + R.drawable.demovideo.mp4); // this line doesnt work. figure out alternative.
+
+        videoView.start();
+
+
+    }
+}

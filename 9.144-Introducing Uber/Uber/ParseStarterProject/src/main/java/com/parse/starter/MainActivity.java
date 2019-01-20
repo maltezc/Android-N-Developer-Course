546973f341526db37523897487bcc6060b46af0e
cflux1990 <cflux.maltez@live.com>
2019-01-20 08:12:36 -0800

    9.147 - Uber Login Page

--- 9.144-Introducing Uber/Uber/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ 9.144-Introducing Uber/Uber/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -25,11 +25,63 @@ import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
+  public void getStarted(View view) {
+
+    Switch userTypeSwitch = (Switch) findViewById(R.id.userTypeSwitch);
+
+    Log.i("Switch value", String.valueOf(userTypeSwitch.isChecked()));
+
+    String userType = "rider";
+
+    if (userTypeSwitch.isChecked()) {
+
+      userType = "driver";
+
+    }
+
+    ParseUser.getCurrentUser().put("riderOrDriver", userType);
+
+    Log.i("Info", "Redirecting as " + userType);
+
+  }
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    getSupportActionBar().hide(); // hides action bar
+
+    if (ParseUser.getCurrentUser() == null) {
+
+      ParseAnonymousUtils.logIn(new LogInCallback() {
+        @Override
+        public void done(ParseUser user, ParseException e) {
+
+          if (e == null) {
+
+            Log.i("Info", "Anonymous Login successful");
+
+          } else {
+
+            Log.i("Info", "Anonymous Login Failed");
+
+          }
+
+        }
+      });
+
+
+    } else {
+
+      if (ParseUser.getCurrentUser().get("riderorDriver") != null) {
+
+        Log.i("Info", "Redirecting as " + ParseUser.getCurrentUser().get("riderOrDriver"));
+
+      }
+
+    }
 
     
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
